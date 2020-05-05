efee772faa47b0f79d5e8f3fe7a9be41239796b4
ankitaku2019 <ankitaku@andrew.cmu.edu>
2020-05-04 20:08:22 -0500

    Fixed some formatting on the project list page

--- assets/main.js
+++ assets/main.js
@@ -1,19 +1,15 @@
 //The code below makes it seem like the hover method changes the color
 //to orange.
 //Instead the code is simply hiding the image to display the orange background
-$(document).ready(function(){
-  $("#firstCard").hover(function(){
-    $(".project-listing-img").css("opacity", "0");
-  }, function(){
- $(".project-listing-img").css("opacity", "100");
-  });
-});
-
-//Hides the project text until hover
-$(document).ready(function(){
-  $("#firstCard").hover(function(){
-    $(".project-listing").css("opacity", "0");
-  }, function(){
- $(".project-listing-img").css("opacity", "100");
-  });
-});
+// $(document).ready(function(){
+//   $("#firstCard").hover(function(){
+//     $(".project-listing-img").css("opacity", "0");
+//     $(".project-title").css("opacity", "0");
+//     $(".project-text").css("opacity", "100");
+//     $(".card-link").css("opacity", "100");
+//   }, function(){
+//  $(".project-listing-img").css("opacity", "100");
+//  $(".project-text").css("opacity", "0");
+//  $(".card-link").css("opacity", "0");
+//   });
+// });
